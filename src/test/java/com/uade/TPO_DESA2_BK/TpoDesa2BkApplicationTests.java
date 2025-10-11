b0eea1b367a87b808fd05ca35f0d9efc613fbcb6
Adriel Fierro <adriel@Adriels-MacBook-Pro.local>
2025-10-11 13:44:01 -0300

    fix: controller casing and add web starter

---
+++ src/test/java/com/uade/TPO_DESA2_BK/TpoDesa2BkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.TPO_DESA2_BK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpoDesa2BkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
